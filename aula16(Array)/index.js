84aa6592c32fa17d50d07d6db22f9efbfbfb8f8b
Pedro Henrique <pedrogoncalves2306@gmail.com>
2023-05-17 17:38:12 -0300

    Aula 16 sobre arrays

---
+++ aula16(Array)/index.js
@@ -0,0 +1,29 @@
+// posições        0      1         2
+const nomes = ['Pedro', 'João', 'Carlos'];
+
+
+console.log(nomes);
+
+console.log(nomes.slice(0,3));
+
+
+
+/*
+nomes.unshift("Luan"); // ADICIONA UM VALOR NO INICIO DO ARRAY
+nomes.push('teste');   // ADICIONA UM VALOR NO FINAL DO ARRAY
+nomes.shift();         // REMOVE O PRIMEIRO ELEMENTO DO ARRAY
+nomes.pop();           // REMOVE O ÚLTIMO ELEMENTO DO ARRAY
+
+
+
+
+console.log(nomes[2]) // ACESSAR UM INDICE DO ARRAY
+
+// ALTERAR UM VALOR
+nomes[0] = "Alterado";
+console.log(nomes);
+
+console.log(nomes.length);   
+*/
+
+
